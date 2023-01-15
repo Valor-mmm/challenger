d3eb3d75dd09f9382f8c7da3fe8a14716e1e7df8
Stefan Hauser <stefan.hauser@tutanota.com>
2023-01-15 23:05:15 +0100

    Add DB Schema for Challenger

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,100 @@
 import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
 async function seed() {
-  const email = "rachel@remix.run";
 
   // cleanup the existing database
-  await prisma.user.delete({ where: { email } }).catch(() => {
+  await prisma.user.deleteMany().catch(() => {
     // no worries if it doesn't exist yet
-  });
+  })
 
-  const hashedPassword = await bcrypt.hash("racheliscool", 10);
-
-  const user = await prisma.user.create({
+  const template1 = await prisma.template.create({
     data: {
-      email,
-      password: {
-        create: {
-          hash: hashedPassword,
-        },
-      },
+      name: 'Test template 1'
+    }
+  })
+
+  const template2 = await prisma.template.create({
+    data: {
+      name: 'Test template 2'
+    }
+  })
+
+  const challengePointMap1 = await prisma.challengePointMap.create({
+    data: {
+      challenge: '45m',
+      points: 2,
+
+      templateId: template1.id
+    }
+  })
+
+  const challengePointMap2 = await prisma.challengePointMap.create({
+    data: {
+      challenge: '90m',
+      points: 3,
+
+      templateId: template1.id
+    }
+  })
+
+  const challengePointMap3 = await prisma.challengePointMap.create({
+    data: {
+      challenge: 'default',
+      points: 2,
+
+      templateId: template2.id
+    }
+  })
+
+  const user1 = await prisma.user.create({
+    data: {
+      name: 'Test User 1',
+      email: 'a@b.com',
+
+      isAdmin: false
     },
   });
 
-  await prisma.note.create({
+  const user2 = await prisma.user.create({
     data: {
-      title: "My first note",
-      body: "Hello, world!",
-      userId: user.id,
+      name: 'Test User 2',
+      email: 'c@d.com',
+
+      isAdmin: false
     },
   });
 
-  await prisma.note.create({
+  const adminUser = await prisma.user.create({
     data: {
-      title: "My second note",
-      body: "Hello, world!",
-      userId: user.id,
+      name: 'Admin User',
+      email: 'ad@min.com',
+
+      isAdmin: true
     },
   });
+
+  const activiy1 = await prisma.activity.create({
+    data: {
+      userId: user1.id,
+      challengePointMapId: challengePointMap1.id
+    }
+  })
+
+  const activiy2 = await prisma.activity.create({
+    data: {
+      userId: user1.id,
+      challengePointMapId: challengePointMap3.id
+    }
+  })
+
+  const activiy3 = await prisma.activity.create({
+    data: {
+      userId: adminUser.id,
+      challengePointMapId: challengePointMap2.id
+    }
+  })
 
   console.log(`Database has been seeded. 🌱`);
 }
